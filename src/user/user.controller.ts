899b91d3b575ecccac260b3e0a277e60888d6e67
juyoung07 <yunju5090@gmail.com>
2025-06-08 22:50:45 +0900

    [feat] : user 정보 업데이트 기능 구현

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,17 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiBody, ApiResponse } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { UserInfo } from '../auth/decorators/user-info.decorator';
 import { JwtAuthGuard } from 'src/config/jwt.config';
+import { UpdateUserInfoDto } from './dto/update-user.dto';
 
 @UseGuards(JwtAuthGuard)
 @ApiTags('Users API')
@@ -20,5 +29,17 @@ export class UserController {
   @ApiResponse({ status: 500, description: '유저 정보 조회 실패' })
   async findUserById(@UserInfo('uid') uid: string) {
     return this.userService.findUserByUid(uid);
+  }
+
+  @Patch()
+  @ApiOperation({
+    summary: '회원 정보 수정',
+    description: '회원 정보를 수정합니다',
+  })
+  @ApiBody({ type: UpdateUserInfoDto })
+  @ApiResponse({ status: 200, description: '수정 성공' })
+  @ApiResponse({ status: 500, description: '수정 실패' })
+  async register(@UserInfo('uid') uid: string, @Body() dto: UpdateUserInfoDto) {
+    return this.userService.update(uid, dto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,6 +7,7 @@ import {
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import { UpdateUserInfoDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -21,5 +22,27 @@ export class UserService {
 
   async findUserByUid(uid: string): Promise<User> {
     return await this.userRepository.findOneBy({ uid });
+  }
+
+  async update(uid: string, dto: UpdateUserInfoDto) {
+    const user = await this.findUserByUid(uid);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    try {
+      const updatedUser = {
+        ...user,
+        ...dto,
+      };
+      await this.userRepository.save(updatedUser);
+      return await this.findUserByUid(uid);
+    } catch (err) {
+      console.log(err);
+      if (err instanceof NotFoundException) {
+        throw err;
+      }
+      throw new InternalServerErrorException('Failed to update user info');
+    }
   }
 }

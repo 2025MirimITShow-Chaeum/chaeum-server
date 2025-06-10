57e53afb358464b181cd4ace8fe78d6fac17a782
An minsun <elleto123456@gmail.com>
2025-06-10 09:04:47 +0900

    ✨feat : 그룹 생성, 유저 그룹 조회,  참가,  단일 그룹 조회, 정보 수정 기능

---
+++ src/groups/dto/create-group.dto.ts
@@ -0,0 +1,18 @@
+import { IsNotEmpty, IsString, Matches, IsUUID } from 'class-validator';
+
+export class CreateGroupDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  subject: string;
+
+  @IsString()
+  @Matches(/^#[0-9A-Fa-f]{6}$/, { message: 'HEX 형식의 색상 코드여야 합니다.' })
+  color: string;
+
+  @IsUUID()
+  user_id: string;
+}
